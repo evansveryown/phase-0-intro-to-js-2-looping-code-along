8dd7325252c9d232a885e3cb7c7f0bbd4883cf83
Cassy Omondi <cksparks07@gmail.com>
2025-06-14 17:57:04 +0300

    Pushed changes to repository

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+  let messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    messages.push(message);
+  }
+
+  return messages;
+}
+
+// Example call:
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+// Example call:
+countDown(10);
